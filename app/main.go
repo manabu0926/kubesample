b85bc9b95d8785f23d0199f993f4684fe4fb8818
栗田学 <snoopy.mk90@gmail.com>
2021-12-23 09:22:11 +0900

    first commit

---
+++ app/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+)
+
+// Embed htmlファイルに埋め込むデータ構造体
+type Embed struct {
+	Title   string
+	Message string
+	Time    time.Time
+}
+
+var templates = make(map[string]*template.Template)
+
+func main() {
+	fmt.Println("Hello golang from docker!")
+	log.Printf("Welcome to go index")
+	port := "8080"
+
+	templates["index"] = loadTemplate("index")
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+
+	http.HandleFunc("/", handleIndex)
+	log.Printf("Server listening on http://localhost:%s/", port)
+	log.Print(http.ListenAndServe(":"+port, nil))
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello golang from index!")
+	log.Printf("Welcome to go index")
+	temp := Embed{"Hello Golang!", "こんにちは！", time.Now()}
+	if err := templates["index"].Execute(w, temp); err != nil {
+		log.Printf("failed to execute template: %v", err)
+	}
+}
+
+func loadTemplate(name string) *template.Template {
+	t, err := template.ParseFiles(
+		"root/"+name+".html",
+		"root/template/header.html",
+		"root/template/footer.html",
+	)
+	if err != nil {
+		log.Fatalf("template error: %v", err)
+	}
+	return t
+}
